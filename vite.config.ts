79045b91410af18c1187d3d0dd178b0e02025246
yohei.hokari <yohei.hokari@gmail.com>
2025-08-20 22:58:17 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@domain': '/src/domain',
+      '@application': '/src/application',
+      '@infrastructure': '/src/infrastructure',
+      '@presentation': '/src/presentation'
+    }
+  }
+})
